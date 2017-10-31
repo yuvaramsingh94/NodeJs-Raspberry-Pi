61bc17b68a6e820076c782e304f0eaeddc56465e
yuvaram singh <yuvaramsingh94@gmail.com>
2017-10-31 18:22:06 +0530

    Update Install-Node.sh

--- Install-Node.sh
+++ Install-Node.sh
@@ -4,11 +4,11 @@ PICHIP=$(uname -m);
 if [ "$EUID" -ne 0 ]
         then echo "You need to install as root by using sudo ./Install-Node.sh";
         exit
-else LINKTONODE=$(curl -G https://nodejs.org/dist/latest-v8.x/ | awk '{print $2}' | grep -P 'href=\"node-v8\.\d{1,}\.\d{1,}-linux-'$PICHIP'\.tar\.gz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//');
+else LINKTONODE=$(curl -G https://nodejs.org/dist/latest-v6.x/ | awk '{print $2}' | grep -P 'href=\"node-v6\.\d{1,}\.\d{1,}-linux-'$PICHIP'\.tar\.gz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//');
 # curl -G https://nodejs.org/dist/latest-v7.x/ | awk '{print $2}' | grep -P 'href=\"node-v7\.\d{1,}\.\d{1,}-linux-armv7l\.tar\.gz' | sed 's/href="//' | sed 's/<\/a>//' | sed 's/">.*//'
 NODEFOLDER=$(echo $LINKTONODE | sed 's/.tar.gz/\//');
 #Next, Creates directory for downloads, and downloads node 8.x
-cd ~/ && mkdir tempNode && cd tempNode && wget https://nodejs.org/dist/latest-v8.x/$LINKTONODE;
+cd ~/ && mkdir tempNode && cd tempNode && wget https://nodejs.org/dist/latest-v6.x/$LINKTONODE;
 tar -xzf $LINKTONODE;
 #Remove the tar after extracing it.
 rm $LINKTONODE;
